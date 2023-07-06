036c32bb6b86bad1548a2894418a10115e2858f5
Caleb Han <102847271+kidscoots101@users.noreply.github.com>
2023-07-06 21:36:45 +0800

    react-router

--- src/App.js
+++ src/App.js
@@ -1,53 +1,16 @@
-import React, { useState, useEffect } from "react";
-import QRCode from "react-qr-code";
-import { initializeApp } from "firebase/app";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { GoogleLogin } from "@harisenin/react-google-login";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { gapi } from "gapi-script";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AttendanceSystem from "./Login";
+import Qr from "./Qr";
 
-const AttendanceSystem = () => {
-  const clientId =
-    "635818492905-f30iuhv6kjtvo08fv8juq468mr6nj7u6.apps.googleusercontent.com";
-
-  const responseGoogle = (response) => {
-    window.location.href = "/Qr.js";
-    console.log("Login Success");
-  };
-  const handleLoginFailure = (error) => {
-    console.log("Login failed:", error);
-    // Treat the login as successful and proceed
-    // window.location.href = "/Qr.js";
-  };
-  gapi.load("client:auth2", () => {
-    gapi.client.init({
-      clientId:
-        "635818492905-f30iuhv6kjtvo08fv8juq468mr6nj7u6.apps.googleusercontent.com",
-      plugin_name: "chat",
-    });
-  });
+export default function App() {
   return (
-    <div
-      className="qr"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      {/* <GoogleOAuthProvider clientId="635818492905-f30iuhv6kjtvo08fv8juq468mr6nj7u6.apps.googleusercontent.com"> */}
-      <GoogleLogin
-        clientId="635818492905-f30iuhv6kjtvo08fv8juq468mr6nj7u6.apps.googleusercontent.com"
-        buttonText="Sign in with Google"
-        onSuccess={responseGoogle}
-        onFailure={handleLoginFailure}
-        cookiePolicy={"single_host_origin"}
-      />
-      {/* </GoogleOAuthProvider> */}
-    </div>
+    <Router>
+      {/* <h1>App</h1> */}
+      <Routes>
+        <Route path="/" element={<AttendanceSystem />} />
+        <Route path="/Qr" element={<Qr />} />
+      </Routes>
+    </Router>
   );
-};
-
-export default AttendanceSystem;
+}
