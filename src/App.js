c5ca981d6c770f242191a15eb4b464c84877a427
Caleb Han <102847271+kidscoots101@users.noreply.github.com>
2023-07-06 18:33:08 +0800

    firebase popup

--- src/App.js
+++ src/App.js
@@ -2,138 +2,30 @@ import React, { useState, useEffect } from "react";
 import QRCode from "react-qr-code";
 import { initializeApp } from "firebase/app";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { GoogleLogin } from "@react-oauth/google";
+import { GoogleLogin } from "@harisenin/react-google-login";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { findAllByAltText } from "@testing-library/react";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDSaQQGTab4XkyTDcVxA4s07m3N8KlYD7k",
-  authDomain: "https://incterminal-88156.web.app/",
-  projectId: "incterminal-88156",
-  storageBucket: "incterminal-88156.appspot.com",
-  messagingSenderId: "635818492905",
-  appId: "1:635818492905:web:8f321bbe5bef7800078178",
-  measurementId: "G-0VY14L4ZM9",
-};
-
-const provider = new GoogleAuthProvider();
-
-//Init Firebase
-const app = initializeApp(firebaseConfig);
-
-//Init Firebase Auth
-const auth = getAuth(app);
-signInWithPopup(auth, provider)
-  .then((result) => {
-    //This gives you a GAuth Token
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    //Signed in user's info
-    const user = result.user;
-    console.log(user);
-  })
-  .catch((error) => {
-    //Handle error
-    const errorCode = error.code;
-    const errorMEssage = error.message;
-    //THE EMAIL THAT WE NEED
-    const email = error.customData.email;
-    //Auth type used
-    const credential = GoogleAuthProvider.credentialFromError(error);
-  });
-
-provider.setCustomParameters({
-  login_hint: "user@s202x.ssts.edu.sg",
-});
-
-function handleDOMContentLoaded() {
-  // Your code to be executed when the website is opened
-
-  console.log("Website opened!");
-}
-
-document.addEventListener("DOMContentLoaded", handleDOMContentLoaded);
+import { gapi } from "gapi-script";
 
 const AttendanceSystem = () => {
-  const [qrCodeData, setQRCodeData] = useState("");
-
-  let timer;
-  useEffect(() => {
-    generateQRCode();
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  const generateQRCode = () => {
-    const studentInfo = getStudentInfo();
-    setQRCodeData(studentInfo);
-    startTimer();
-  };
-
-  const startTimer = () => {
-    setTimeout(() => {
-      generateQRCode();
-    }, 10000);
-  };
-
-  function rot13(text) {
-    return text.replace(/[a-zA-Z]/g, function (c) {
-      var charCode = c.charCodeAt(0);
-      var base = charCode < 91 ? 65 : 97;
-      return String.fromCharCode(((charCode - base + 13) % 26) + base);
-    });
-  }
-
-  function base64Encode(text) {
-    return btoa(text);
-  }
-  function knitString(str) {
-    let result = "";
-    let left = 0;
-    let right = str.length - 1;
-
-    while (left <= right) {
-      if (left === right) {
-        result += str[left];
-      } else {
-        result += str[left] + str[right];
-      }
-      left++;
-      right--;
-    }
-
-    return result;
-  }
-
-  function encryptText(text) {
-    var knitted = knitString(knitString(text));
-    var base64Text = base64Encode(knitted);
-    var rot13Text = rot13(base64Text);
-    var finalText = base64Encode(rot13Text);
-    console.log(finalText);
-  }
-
-  const getStudentInfo = () => {
-    var currentTime = Math.floor(Date.now() / 1000);
-    var finalouput = encryptText("example@gmail.com" + currentTime);
-
-    const studentInfo = `${finalouput}`;
-
-    return studentInfo;
-  };
   const clientId =
     "635818492905-f30iuhv6kjtvo08fv8juq468mr6nj7u6.apps.googleusercontent.com";
 
-  const handleLoginSuccess = (response) => {
-    // Access the email address from the response object
-    const email = response.profileObj.email;
-    console.log(email);
+  const responseGoogle = (response) => {
+    window.location.href = "/Qr.js";
+    console.log("Login Success");
   };
-
-  const handleLoginFailure = () => {
-    console.log("Login Failed");
+  const handleLoginFailure = (error) => {
+    console.log("Login failed:", error);
+    // Treat the login as successful and proceed
+    // window.location.href = "/Qr.js";
   };
-
+  gapi.load("client:auth2", () => {
+    gapi.client.init({
+      clientId:
+        "635818492905-f30iuhv6kjtvo08fv8juq468mr6nj7u6.apps.googleusercontent.com",
+      plugin_name: "chat",
+    });
+  });
   return (
     <div
       className="qr"
@@ -145,17 +37,15 @@ const AttendanceSystem = () => {
         height: "100vh",
       }}
     >
-      {/* {qrCodeData && <QRCode value={qrCodeData} size={200} key={qrCodeData} />} */}
-      <GoogleOAuthProvider clientId="635818492905-f30iuhv6kjtvo08fv8juq468mr6nj7u6.apps.googleusercontent.com">
-        <GoogleLogin
-          clientId={clientId}
-          onSuccess={handleLoginSuccess}
-          onFailure={handleLoginFailure}
-          scope="https://www.googleapis.com/auth/userinfo.email"
-          buttonText="Login with Google"
-          cookiePolicy={"single_host_origin"}
-        />
-      </GoogleOAuthProvider>
+      {/* <GoogleOAuthProvider clientId="635818492905-f30iuhv6kjtvo08fv8juq468mr6nj7u6.apps.googleusercontent.com"> */}
+      <GoogleLogin
+        clientId="635818492905-f30iuhv6kjtvo08fv8juq468mr6nj7u6.apps.googleusercontent.com"
+        buttonText="Sign in with Google"
+        onSuccess={responseGoogle}
+        onFailure={handleLoginFailure}
+        cookiePolicy={"single_host_origin"}
+      />
+      {/* </GoogleOAuthProvider> */}
     </div>
   );
 };
