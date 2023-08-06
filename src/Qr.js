14559178f128506778e758b7b25354fb4aa9b50b
Tristan <93832702+cheesetan@users.noreply.github.com>
2023-08-06 11:47:14 +0800

    updated text when scanned qr code

--- src/Qr.js
+++ src/Qr.js
@@ -285,7 +285,7 @@ export default function Qr() {
           fontSize: isSmallScreen ? "18px" : "23px",
           paddingBottom: isSmallScreen ? "15px" : "30px",
           paddingLeft: isSmallScreen ? "18px" : "23px",
-      }}>Attendance has been taken! Thank you for keeping SST Inc. #INCredible</text>
+      }}>Attendance submitted! Please check the QR Code terminal to ensure that it was successful taken. Thank you for keeping SST Inc. #INCredible</text>
 
       )}
       
