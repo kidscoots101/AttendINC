8da62a85ec603c2775c56b6510ed7d8c52c9a392
Caleb Han <102847271+kidscoots101@users.noreply.github.com>
2023-08-26 19:10:44 +0800

    added privacy policy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AttendanceSystem from "./Login";
 import Qr from "./Qr";
+import Modal from 'react-modal';
+import './App.css'
+import Policy from "./Policy";
 
 export default function App() {
+  const [showFullPolicy, setShowFullPolicy] = useState(false);
+
+  useEffect(() => {
+    const hasSeenPolicy = localStorage.getItem('hasSeenPolicy');
+
+    if (!hasSeenPolicy) {
+      setShowFullPolicy(true);
+    }
+  }, []);
+
+  const handleAccept = () => {
+    localStorage.setItem('hasSeenPolicy', 'true');
+    setShowFullPolicy(false);
+  };
+
   return (
     <Router>
-      {/* <h1>App</h1> */}
+     <Modal
+        isOpen={showFullPolicy}
+        onRequestClose={setShowFullPolicy}
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <div className="modal-content">
+          <h2>UPDATE!</h2>
+          <p>Our privacy policy has been updated! Please read our <a href="/privacy-policy">Privacy Policy</a>.</p>
+          <button onClick={handleAccept}>Got it!</button>
+        </div>
+      </Modal>
       <Routes>
         <Route path="/" element={<AttendanceSystem />} />
         <Route path="/Qr" element={<Qr />} />
+        <Route path="/privacy-policy" element={<Policy />} />
+
       </Routes>
     </Router>
   );
