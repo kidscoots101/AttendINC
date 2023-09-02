4f7229821920a441a0198df8d2183a36375ed46b
Tristan <tristanchay123@gmail.com>
2023-09-02 14:48:44 +0800

    fixed link redirect

--- src/Config.js
+++ src/Config.js
@@ -4,14 +4,14 @@ import { useLocation } from "react-router-dom";
 function ConfigPage() {
   const location = useLocation();
 
-  const configParam = new URLSearchParams(location.search).get("config");
+  const configParam = new URLSearchParams(location.search);
 
   const openMacApp = () => {
     const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
     if (isSafari) {
       window.location.href = 'attendinc://'; 
     } else {
-      window.open('attendinc://' + "config?=" + configParam);
+      window.open('attendinc://' + "config?" + configParam);
     }
   };
 
