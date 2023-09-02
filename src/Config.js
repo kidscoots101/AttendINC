1a96c8c5e397a00182e50c6a1222b99d5cb214d5
Caleb Han <102847271+kidscoots101@users.noreply.github.com>
2023-09-02 11:27:58 +0800

    added config.js

---
+++ src/Config.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+function ConfigPage() {
+  const location = useLocation();
+
+  const configParam = new URLSearchParams(location.search).get("config");
+
+  const openMacApp = () => {
+    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    if (isSafari) {
+      window.location.href = 'attendinc://'; 
+    } else {
+      window.open('attendinc://' + "config?=" + configParam);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Config Details</h2>
+      <p>Config: {configParam}</p>
+      <button onClick={openMacApp}>Open Mac App</button>
+    </div>
+  );
+}
+
+export default ConfigPage;
