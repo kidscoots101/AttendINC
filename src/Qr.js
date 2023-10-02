dd621a3a1bf78cc81ebc463f4672f37c3f22b8e7
Caleb Han <calebhan08js@gmail.com>
2023-10-02 18:27:09 +0800

    header igm

--- src/Qr.js
+++ src/Qr.js
@@ -183,6 +183,8 @@ export default function Qr() {
     window.location.reload();
   };
 
+  
+
   function ScannerArea(props) {
     const credentialsAreValid = props.credentialsAreValid;
     if (isScanned) {
